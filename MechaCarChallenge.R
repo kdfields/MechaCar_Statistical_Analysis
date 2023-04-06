c43e9bc8086052a29492fea7804a73bef0494d90
Kyle Fields <113741694+kdfields@users.noreply.github.com>
2023-04-06 15:25:52 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,33 +4,33 @@ library(dplyr)
 # import MechaCar_mpg.csv
 mpg <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
 
-# generate multiple linear regression model
+# Part1: Generate multiple linear regression model for all six variables
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mpg)
 
-# generate summary statistics
+# Part1: Create statistical summary of the linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mpg))
 
 # import Suspension_Coil.csv
 coil <- read.csv('Suspension_coil.csv',check.names = F,stringsAsFactors = F)
 
-# Create total_summary dataframe for Suspension_Coil
+# Part2: Create total_summary dataframe for Suspension_Coil
 coil_summary <- coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 
-# Create lot_summary dataframe for Suspension_Coil by lot
+# Part2: Create lot_summary dataframe for Suspension_Coil by lot
 lot_summary <- coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 
 #generate 50 randomly sampled data points from coil
 sample_table <- coil %>%  sample_n(50)
 
-# compare all lots versus population mean
+# Part3: write an RScript using the t.test() function to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch.
 t.test(coil$PSI,mu=1500)
 
-# filtering indivual lots to compare to population mean of 1500
+# Part3: Create lot summary dataframes for each of the 3 Lots
 coil_psi_lot1 <- coil %>% filter(Manufacturing_Lot == 'Lot1')
 coil_psi_lot2 <- coil %>% filter(Manufacturing_Lot == 'Lot2')
 coil_psi_lot3 <- coil %>% filter(Manufacturing_Lot == 'Lot3')
 
-# t-test to compare each lot's mean to population mean of 1500
+# Part3, cont: write three more RScripts in your MechaCarChallenge.RScript using the t.test() function and its subset() argument
 t.test(coil_psi_lot1$PSI,mu=1500)
 t.test(coil_psi_lot2$PSI,mu=1500)
 t.test(coil_psi_lot3$PSI,mu=1500)
